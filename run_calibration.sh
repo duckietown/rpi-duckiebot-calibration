1a44c6facc91cad6842cc8511dae0adf8992ef4b
Ruslan Hristov <rusi@nutonomy.com>
2018-07-05 18:14:15 -0400

    updated calibration script with instructions

--- run_calibration.sh
+++ run_calibration.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 set -eu
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
-rosrun complete_image_pipeline calibrate_extrinsics -o /data/out-calibrate-extrinsics-$TIMESTAMP
-rosrun complete_image_pipeline single_image_pipeline -o /data/out-pipeline-$TIMESTAMP
+NAME="out-calibrate-extrinsics-$TIMESTAMP"
+VNAME="out-pipeline-$TIMESTAMP"
+echo "********************"
+echo "Place the Duckiebot on the calibration patterns and press ENTER."
+read
+rosrun complete_image_pipeline calibrate_extrinsics -o /data/$NAME > /data/$NAME.log
+
+echo "********************"
+echo "Place the Duckiebot in a lane and press ENTER."
+read
+rosrun complete_image_pipeline single_image_pipeline -o /data/$NAME > /data/$VNAME.log
+
+echo "DONE. Review images under:"
+echo "     /data/$NAME"
+echo "     /data/$VNAME"
+echo "If you have rpi-simple-server running, go to http://<DUCKIEBOT_NAME>.local:8080"
+
