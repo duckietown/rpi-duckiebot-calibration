0a9c85905c92911af893c333f1ba6dbc353fa7e8
lpaull <lpaull@mit.edu>
2018-09-27 10:50:19 -0400

    we are no longer running the extrinsic cal from inside this container

--- run_calibration.sh
+++ run_calibration.sh
@@ -13,17 +13,3 @@ echo "Position the checkerboard pattern in front of the Duckiebot and press ENTE
 read
 roslaunch duckietown intrinsic_calibration.launch veh:=$ROS_MASTER
 
-echo "********************"
-echo "Place the Duckiebot on the calibration patterns and press ENTER."
-read
-rosrun calibrate_extrinsics -o /data/$NAME > /data/$NAME.log
-
-echo "********************"
-echo "Place the Duckiebot in a lane and press ENTER."
-read
-rosrun single_image_pipeline -o /data/$VNAME > /data/$VNAME.log
-
-echo "DONE. Review images under:"
-echo "     /data/$NAME"
-echo "     /data/$VNAME"
-echo "If you have duckietown/rpi-simple-server running, visit http://$HOSTNAME.local:8082/"
